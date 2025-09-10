a66947e05ba536a36c2ce7632ff4280c30b80aee
stepan_romankov <stepan@romankov.name>
2025-09-10 16:20:12 +0200

    buf.build/bufbuild/protovalidate

--- api/gen/proto/tzero/v1/common/payment_method.pb.go
+++ api/gen/proto/tzero/v1/common/payment_method.pb.go
@@ -7,7 +7,7 @@
 package common
 
 import (
-	_ "github.com/t-0-network/provider-sdk-go/api/gen/proto/buf/validate"
+	_ "buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
